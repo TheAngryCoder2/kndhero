dc72f2b7f7b166f019e343c058735754ec0fd90e
WhosRaph <79235452+WhosRaph@users.noreply.github.com>
2024-10-16 19:46:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let btnOne = document.getElementById("one");
+let btnTwo = document.getElementById("two");
+let btnThree = document.getElementById("three");
+let btnFour = document.getElementById("four");
+let btnFive = document.getElementById("five");
+let numbuh = document.getElementById("numbuh");
+let name = document.getElementById("name");
+let number = document.getElementById("number");
+let details = document.getElementById("details");
+
+btnOne.addEventListener("click", () => {
+    numbuh.style.backgroundImage = "url(./Kids_Next_Door-logo-08D52E7DD8-seeklogo.com.png)";
+    name.style.color = "rgb(216, 11, 11)";
+    name.textContent = "Nigel Uno";
+    number.textContent = "Numbuh 1";
+    details.textContent = "Nigel Uno a.k.a. Numbuh 1 is the bald, British leader and head operative of Sector V, keeper of the Book of KND and current Earth representative to the elite Galactic KND. Described as a tactical genius by some and as a paranoid workaholic by others, he has risen to become one of the greatest KND operatives ever. Numbuh 1 takes his position and responsibilities as a KND operative very seriously, often engaging in missions for days at a time without rest."
+});
+btnTwo.addEventListener("click", () => {
+    numbuh.style.backgroundImage = "url(./kids-next-door-logo-30587783B3-seeklogo.com.png)";
+    name.style.color = "rgb(15, 187, 255)";
+    name.textContent = "Hogarth Pennywhistle Gilligan Jr.";
+    number.textContent = "Numbuh 2";
+    details.textContent = "Numbuh 2 is an American boy who loves cracking puns at every chance he sees, and eating chili dogs. He is an inventor and pilot with a passion for flight. He is the 2x4 Technology Officer of Kids Next Door Sector V."
+});
+btnThree.addEventListener("click", () => {
+    numbuh.style.backgroundImage = "url(./Dfgl404-e5c4264e-9d16-45cc-93f3-adfe4ad2c399.webp)";
+    name.style.color = "rgb(12, 171, 83)";
+    name.textContent = "Kuki Sanban";
+    number.textContent = "Numbuh 3";
+    details.textContent = "Kuki Sanban (三番茎) a.k.a. Numbuh 3 is a happy-go-lucky, 10-year-old 4th-grade Japanese-American girl who is in charge of Diversionary Tactics and is the Medical Specialist of Sector V. She also takes care of the hamsters that power the treehouse. She is the youngest operative, and one of the two female members (alongside Numbuh 5) and the only Asian-American in her sector. As an adult, she becomes President of the Rainbow Monkey Corporation, and the first thing she did when she took over was tear down a Rainbow Monkey Potty Training amusement park."
+});
+btnFour.addEventListener("click", () => {
+    numbuh.style.backgroundImage = "url(./kids-next-door-logo-86FD9913F2-seeklogo.com.png)";
+    name.style.color = "rgb(231, 149, 8)";
+    name.textContent = "Wallabee 'Wally' Beetles";
+    number.textContent = "Numbuh 4";
+    details.textContent = "Wallabee 'Wally' Beetles a.k.a. Numbuh 4 is the brash and impulsive hand-to-hand combatant Australian boy of the Kids Next Door Sector V. He is close friends with Hoagie and has a crush on his teammate, Kuki, who is his close friend and teammate who returns his feelings. He ends up marrying Kuki in the future. There are few amongst the KND that can match him in hand-to-hand combat, and even fewer who can compete with his bravery. Numbuh 4 is also known for his apparent lack of intelligence, preferring to simply beat up any and all obstacles in his way."
+});
+btnFive.addEventListener("click", () => {
+    numbuh.style.backgroundImage = "url(./Numbuh_5.webp)";
+    name.style.color = "rgb(10, 99, 232)";
+    name.textContent = "Abigail 'Abby' (Gilligan) Lincoln";
+    number.textContent = "Numbuh 5";
+    details.textContent = "Abigail 'Abby' (Gilligan) Lincoln a.k.a. Numbuh 5 is the intelligent, relaxed girl second-in-command/spy in Sector V. She became a KND operative three years before the show started as shown in Operation: M.A.U.R.I.C.E.. She was trained by her older sister, Cree (Numbuh 11), and joined her sister, Numbuhs 8a and 8b, and Numbuh 9 in Sector V. <br> After Cree betrayed and was defected from the KND, Numbuh 5 became the leader of Sector V, until she allowed Numbuh 1 to take over after she found and hacked his grades for his KND entrance exam. She is the oldest member of Sector V. Numbuh 5 later became Supreme Leader after Numbuh 362 retired."
+});
